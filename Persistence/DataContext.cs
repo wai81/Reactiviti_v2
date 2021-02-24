bab4506352488a32dbf4ccf265ab977addd1fdf2
wai81 <50743966+wai81@users.noreply.github.com>
2021-02-24 23:29:57 +0300

    End part 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    //public class DataContext : IdentityDbContext<AppUser>
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+       
+        public DbSet<Activity> Activities { get; set; }
+       
+    }
+}
