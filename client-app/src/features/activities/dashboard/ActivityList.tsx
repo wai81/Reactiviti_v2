274b9883c163163ae629a2499c53ce6232c0d4b4
wai81 <50743966+wai81@users.noreply.github.com>
2021-03-02 01:49:57 +0300

    End part 7

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,15 +1,14 @@
+import { observer } from "mobx-react-lite";
 import React, { SyntheticEvent, useState } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 
-interface Props {
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
 
-export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+export default observer( function ActivityList() {
+
+    const {activityStore} = useStore();
+    const {selectActivity, deleteActivity, activitiesByDate, loading} = activityStore;
+
     const [target, setTarget] = useState('');
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
@@ -21,7 +20,7 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
         <Segment.Group>
             <Segment>
                 <Item.Group>
-                    {activities.map(activity => (
+                    {activitiesByDate.map(activity => (
                         <Item key={activity.id}>
                             <Item.Content>
                                 <Item.Header>{activity.title}</Item.Header>
@@ -34,7 +33,7 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                                     <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
                                     <Button
                                         name = {activity.id}
-                                        loading={submitting && target === activity.id}
+                                        loading={loading && target === activity.id}
                                         onClick={(e) => handleActivityDelete(e, activity.id)}
                                         floated='right'
                                         content='Delete'
@@ -49,4 +48,4 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
             </Segment>
         </Segment.Group>
     )
-}
+})
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,15 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
 import { Button, Card, Image } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useStore } from "../../../app/stores/store";
 
-interface Props {
-    activity: Activity;
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
-}
+export default observer(function ActivityDetails() {
+    const { activityStore } = useStore();
+    const {selectedActivity: activity, cancelSelectedActivity, openForm} =activityStore;
+    
+    if (!activity) return <LoadingComponent />;
 
-export default function ActivityDetails({ activity, cancelSelectActivity, openForm }: Props) {
     return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
@@ -24,9 +25,9 @@ export default function ActivityDetails({ activity, cancelSelectActivity, openFo
             <Card.Content extra>
                 <Button.Group widths='2'>
                     <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
-                    <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
+                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel' />
                 </Button.Group>
             </Card.Content>
         </Card>
     )
-}
+})
